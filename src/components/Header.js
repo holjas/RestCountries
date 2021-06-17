6c8d2c0dc06d0f075c8f68dfb4df0e28bf24fab6
holjas <holly.jasiura@gmail.com>
2021-06-16 21:32:19 -0400

    set up happening

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>Where in the world?</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Navigation.js
@@ -0,0 +1,5 @@
+const Navigation = () => {
+  return <h2> I didn't like the componenet thing</h2>;
+};
+
+export default Navigation;
